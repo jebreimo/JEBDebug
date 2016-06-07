aeb8f22c44566a61909b912768c1c342f9aca39a
Jan Erik Breimo <jebreimo@gmail.com>
2016-06-07 15:12:46 +0200

    Use high_resolution_clock within CpuTimer.

--- JEBDebug/CpuTimer.hpp
+++ JEBDebug/CpuTimer.hpp
@@ -5,103 +5,104 @@
  * This file is distributed under the BSD License.
  * License text is included with the source distribution.
  */
-#ifndef JEBDEBUG_CPUTIMER_HPP
-#define JEBDEBUG_CPUTIMER_HPP
+#pragma once
 
-#include <ctime>
+#include <chrono>
 #include <ostream>
 #include <string>
 
 namespace JEBDebug
 {
+    using std::chrono::high_resolution_clock;
 
-class CpuTimer
-{
-public:
-    CpuTimer();
-    void start();
-    void stop();
-    void reset();
-    double seconds() const;
-    bool stopped() const;
-private:
-    clock_t m_StartTime;
-    clock_t m_AccumulatedTime;
-};
-
-template <typename String>
-class ScopedTimerImpl
-{
-public:
-    typedef typename String::value_type CharT;
-    typedef std::basic_ostream<CharT, std::char_traits<CharT>> Stream;
-
-    ScopedTimerImpl(const String& label, Stream& stream)
-        : m_Label(label),
-          m_Stream(stream)
+    class CpuTimer
     {
-        m_Timer.start();
+    public:
+        CpuTimer();
+        void start();
+        void stop();
+        void reset();
+        double seconds() const;
+        bool stopped() const;
+    private:
+        high_resolution_clock::time_point m_StartTime;
+        high_resolution_clock::duration m_AccumulatedTime;
+        bool m_IsStopped;
+    };
+
+    template <typename String>
+    class ScopedTimerImpl
+    {
+    public:
+        typedef typename String::value_type CharT;
+        typedef std::basic_ostream<CharT, std::char_traits<CharT>> Stream;
+
+        ScopedTimerImpl(const String& label, Stream& stream)
+            : m_Label(label),
+              m_Stream(stream)
+        {
+            m_Timer.start();
+        }
+
+        ~ScopedTimerImpl()
+        {
+            m_Timer.stop();
+            m_Stream << m_Label << m_Timer << "\n";
+        }
+    private:
+        CpuTimer m_Timer;
+        String m_Label;
+        Stream& m_Stream;
+    };
+
+    typedef ScopedTimerImpl<std::string> ScopedTimer;
+    typedef ScopedTimerImpl<std::wstring> ScopedTimerW;
+
+    template <typename Char, typename Traits>
+    std::basic_ostream<Char, Traits>& operator<<(
+            std::basic_ostream<Char, Traits>& os,
+            const CpuTimer& stopwatch)
+    {
+      return os << stopwatch.seconds();
     }
 
-    ~ScopedTimerImpl()
+    inline CpuTimer::CpuTimer()
+        : m_IsStopped(true)
+    {}
+
+    inline void CpuTimer::start()
     {
-        m_Timer.stop();
-        m_Stream << m_Label << m_Timer << "\n";
+        m_StartTime = high_resolution_clock::now();
+        m_IsStopped = false;
     }
-private:
-    CpuTimer m_Timer;
-    String m_Label;
-    Stream& m_Stream;
-};
 
-typedef ScopedTimerImpl<std::string> ScopedTimer;
-typedef ScopedTimerImpl<std::wstring> ScopedTimerW;
+    inline void CpuTimer::stop()
+    {
+        auto endTime = high_resolution_clock::now();
+        m_AccumulatedTime += endTime - m_StartTime;
+        m_IsStopped = true;
+    }
 
-template <typename Char, typename Traits>
-std::basic_ostream<Char, Traits>& operator<<(
-        std::basic_ostream<Char, Traits>& os,
-        const CpuTimer& stopwatch)
-{
-  os << stopwatch.seconds();
-  return os;
+    inline void CpuTimer::reset()
+    {
+        m_AccumulatedTime = high_resolution_clock::duration::zero();
+        m_IsStopped = true;
+    }
+
+    inline double CpuTimer::seconds() const
+    {
+        using namespace std::chrono;
+        auto tmp = m_AccumulatedTime;
+        if (!stopped())
+        {
+            auto endTime = high_resolution_clock::now();
+            tmp += endTime - m_StartTime;
+        }
+        return duration<double>(tmp).count();
+    }
+
+    inline bool CpuTimer::stopped() const
+    {
+        return m_IsStopped;
+    }
 }
-
-inline CpuTimer::CpuTimer()
-    : m_StartTime(0),
-      m_AccumulatedTime(0)
-{
-}
-
-inline void CpuTimer::start()
-{
-    m_StartTime = clock();
-}
-
-inline void CpuTimer::stop()
-{
-    clock_t endTime = std::clock();
-    m_AccumulatedTime += endTime - m_StartTime;
-    m_StartTime = 0;
-}
-
-inline void CpuTimer::reset()
-{
-    m_AccumulatedTime = m_StartTime = 0;
-}
-
-inline double CpuTimer::seconds() const
-{
-    if (stopped())
-        return m_AccumulatedTime / (double)CLOCKS_PER_SEC;
-    else
-        return m_AccumulatedTime + (clock() - m_StartTime) / (double)CLOCKS_PER_SEC;
-}
-
-inline bool CpuTimer::stopped() const
-{
-    return m_StartTime == 0;
-}
-
-}
-
-#endif
