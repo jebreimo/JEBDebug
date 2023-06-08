495c2549fb2c438a5abaa419df825edb450a39a2
Jan Erik Breimo <jan_erikbreimo@trimble.com>
2023-06-08 17:12:53 +0200

    Make the profiler report prettier.

--- include/JEBDebug/Profiler.hpp
+++ include/JEBDebug/Profiler.hpp
@@ -8,12 +8,13 @@
 #pragma once
 
 #include <algorithm>
+#include <cfloat>
 #include <chrono>
-#include <limits>
-#include <map>
+#include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <fstream>
+#include <limits>
+#include <map>
 #include <string_view>
 #include <vector>
 
@@ -142,31 +143,44 @@ namespace JEBDebug
 
         void write(std::ostream& os) const
         {
-            size_t nameWidth = 0;
-            for (auto it = sequence_.begin(); it != sequence_.end(); ++it)
+            auto int_width = [](auto n)
             {
-                nameWidth = std::max(nameWidth,
-                                     (*it)->first.func_name.size());
+                return std::streamsize(std::ceil(std::log10(n)));
+            };
+
+            auto float_width = [](auto n)
+            {
+                return std::streamsize(std::ceil(std::log10(n))) + 5;
+            };
+
+            std::streamsize widths[5] = {};
+            for (const auto& it : sequence_)
+            {
+                widths[0] = std::max(widths[0], int_width(it->second.count()));
+                widths[1] = std::max(widths[1], float_width(it->second.acc_time()));
+                widths[2] = std::max(widths[2], float_width(it->second.min_time()));
+                widths[3] = std::max(widths[3], float_width(it->second.max_time()));
+                widths[4] = std::max(widths[4], std::streamsize(it->first.func_name.size()));
             }
 
-            nameWidth = std::min(nameWidth, size_t(30));
+            os << std::setprecision(4) << std::fixed;
             for (auto it = sequence_.begin(); it != sequence_.end(); ++it)
             {
-                os << std::left << std::setw(std::streamsize(nameWidth))
-                   << (*it)->first.func_name
-                   << std::setprecision(4) << std::fixed
-                   << "  " << (*it)->second.count()
-                   << " " << (*it)->second.acc_time()
-                   << " " << (*it)->second.min_time()
-                   << " " << (*it)->second.max_time()
+                using std::left, std::right, std::setw;
+                os << right << setw(widths[0]) << (*it)->second.count()
+                   << " " << setw(widths[1]) << (*it)->second.acc_time()
+                   << " " << setw(widths[2]) << (*it)->second.min_time()
+                   << " " << setw(widths[3]) << (*it)->second.max_time()
+                   << "  " << left << setw(widths[4]) << (*it)->first.func_name
                    << "  " << (*it)->first.file_name
                    #ifdef _MSC_VER
                    << "(" << (*it)->first.line_no << ")"
                    #else
                    << ":" << (*it)->first.line_no()
                    #endif
-                   << std::endl;
+                   << '\n';
             }
+            os.flush();
         }
 
         void write() const
@@ -225,11 +239,11 @@ namespace JEBDebug
 #ifdef _MSC_VER
     #define JEB_PROFILE() \
         ::JEBDebug::ProfilerTimer JEBPROFILER_UNIQUE_NAME(profile) \
-            (__FILE__, __FUNCSIG__, __LINE__)
+            (__FILE__, __func__, __LINE__)
 #else
     #define JEB_PROFILE() \
         ::JEBDebug::ProfilerTimer JEBPROFILER_UNIQUE_NAME(profile) \
-            (__FILE__, __PRETTY_FUNCTION__, __LINE__)
+            (__FILE__, __func__, __LINE__)
 #endif
 
 #define JEB_PROFILER_REPORT() \
