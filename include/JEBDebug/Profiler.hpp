5989546e09640542e5f3dda0163936c57b5d3a4d
Jan Erik Breimo <jan_erikbreimo@trimble.com>
2023-06-08 15:02:39 +0200

    Improve the name of a macro.

--- include/JEBDebug/Profiler.hpp
+++ include/JEBDebug/Profiler.hpp
@@ -17,7 +17,7 @@
 #include <string_view>
 #include <vector>
 
-#if !defined(JEB_INSTANTIATE_PROFILER) && !defined(JEB_MULTI_UNIT_PROFILER)
+#if !defined(JEB_INSTANTIATE_PROFILER) && !defined(JEB_SHARE_PROFILER)
     #define JEB_INSTANTIATE_PROFILER
 #endif
 
